8cb945313d855793d6453dbb651f4bbfc1226f29
aletxapascual <aletxa.pasacual@gmail.com>
2024-05-19 11:30:44 -0600

    video 11 pt.2

--- Space Ship Game/src/gameObjects/Chronometer.java
+++ Space Ship Game/src/gameObjects/Chronometer.java
@@ -1,32 +1,34 @@
 package gameObjects;
 
 public class Chronometer {
-	private long delta,lastTime;
+	private long delta, lastTime;
 	private long time;
 	private boolean running;
 	
-	public Chronometer() {
-		delta=0;
-		lastTime=0;
-		running=false;
+	public Chronometer(){
+		delta = 0;
+		lastTime = 0;
+		running = false;
 	}
 	
-	public void run(long time) {
-		running=true;
-		this.time=time;
+	public void run(long time){
+		running = true;
+		this.time = time;
 	}
 	
-	public void update() {
+	public void update(){	
 		if(running)
-			delta+=System.currentTimeMillis()-lastTime;
-		if(delta>=time) {
-			running=false;
+			delta += System.currentTimeMillis() - lastTime;
+		if(delta >= time){
+			running = false;
+			delta = 0;
 		}
 		
-		lastTime=System.currentTimeMillis();		
+		lastTime = System.currentTimeMillis();
 	}
 	
-	public boolean isRunning() {
+	public boolean isRunning(){
 		return running;
 	}
+	
 }
